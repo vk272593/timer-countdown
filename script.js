ea4fb6161342ea0a2db6cdd3e9ad7bc421ee25c0
vikash kumar <kumarvikash6101998@gmail.com>
2021-09-20 02:08:31 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const startingMinutes = 10;
+let time = startingMinutes*60;
+const countdownE1 = document.getElementById('countdown');
+
+setInterval(updateCountDown, 1000);
+
+function updateCountDown(){
+    const minutes = Math.floor(time/60);
+    let seconds = time%60;
+    seconds = seconds<10?'0'+seconds:seconds;
+    countdownE1.innerHTML=`${minutes}:${seconds}`;
+    time--;
+}
